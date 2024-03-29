dcbc127d844047414dbabf4c0c1abd506e5361dd
Wolfcoder13 <wolfninja13@gmail.com>
2024-03-29 11:18:04 +0000

    extra check for ball location to avoid null pointer

--- src/components/StreamInterface/StreamInterface.js
+++ src/components/StreamInterface/StreamInterface.js
@@ -448,12 +448,12 @@ class StreamInterface extends Component {
         }
 
         let fieldDomination = [...this.state.fieldDomination];
-        if (game && game.winner === "" && game.ball.location.Y < 0 && !game.isReplay) {
+        if (game && game.winner === "" && game.ball.location && game.ball.location.Y < 0 && !game.isReplay) {
           fieldDomination[0] += 1;
           this.setState({
             fieldDomination: fieldDomination
           });
-        } else if (game && game.winner === "" && game.ball.location.Y > 0 && !game.isReplay) {
+        } else if (game && game.winner === "" && game.ball.location && game.ball.location.Y > 0 && !game.isReplay) {
           fieldDomination[1] += 1;
           this.setState({
             fieldDomination: fieldDomination
